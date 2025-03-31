c1235ccb5b39d19c2c28d96bf7b8e4f27fc34a50
Daniel Naro <dnaro@ac.upc.edu>
2025-03-31 12:31:51 +0200

    Changing CORS options

--- beacon/conf/conf.py
+++ beacon/conf/conf.py
@@ -27,7 +27,8 @@ max_beacon_granularity = "record" # boolean, count or record
 security_levels = ['PUBLIC', 'REGISTERED', 'CONTROLLED']
 documentation_url = 'https://b2ri-documentation-demo.ega-archive.org/'
 alphanumeric_terms = ['libraryStrategy', 'molecularAttributes.geneIds', 'diseases.ageOfOnset.iso8601duration', 'molecularAttributes.aminoacidChanges','phenotypicFeatures.onset.iso8601duration', 'exposures.ageAtExposure.iso8601duration', 'treatments.ageAtOnset.iso8601duration']
-cors_urls = ["http://localhost:3000","https://cancer-beacon-demo.ega-archive.org", "https://beacon-network-demo2.ega-archive.org", "https://beacon.ega-archive.org"]
+cors_headers_exempt_urls = ["http://localhost:5050", "http://localhost:3000"]
+cors_urls = ["http://localhost:3000","http://localhost:5050","https://cancer-beacon-demo.ega-archive.org", "https://beacon-network-demo2.ega-archive.org", "https://beacon.ega-archive.org"]
 test_datasetId="synthetic_usecases_4beacon_testingV3"
 
 # Service Info
